15eeb3f98a084717ec9be52ac4918afaafe8d357
ran_liu <kn880701@gmail.com>
2025-06-12 14:24:57 +0800

    [refactor] 移除無用檔案

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -6,7 +6,6 @@ class AppImages {
   static const arrowDownIcon = '${assetsIcons}ic_arrow_down.svg';
   static const arrowLeftIcon = '${assetsIcons}ic_arrow_left.svg';
   static const arrowRightIcon = '${assetsIcons}ic_arrow_right.svg';
-  static const cancelIcon = '${assetsImages}ic_cancel.png';
   static const editIcon = '${assetsIcons}ic_edit.svg';
 
   static const appleIcon = '${assetsImages}ic_apple.png';
@@ -18,8 +17,6 @@ class AppImages {
   static const addIcon = '${assetsImages}ic_add.png';
   static const homeActiveIcon = '${assetsImages}ic_home_active.png';
   static const homeInactiveIcon = '${assetsImages}ic_home_inactive.png';
-  static const notifyActiveIcon = '${assetsImages}ic_notify_active.png';
-  static const notifyInactiveIcon = '${assetsImages}ic_notify_inactive.png';
   static const personalActiveIcon = '${assetsImages}ic_personal_active.png';
   static const personalInactiveIcon = '${assetsImages}ic_personal_inactive.png';
   static const listIcon = '${assetsImages}ic_list.png';
